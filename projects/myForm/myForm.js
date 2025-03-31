78a6ef47c054a84f9b65b64d0b1b0a288d42d1c5
Laurence Castro <lrncecastro@gmail.com>
2025-03-31 12:42:21 -1000

    update

---
+++ projects/myForm/myForm.js
@@ -0,0 +1,64 @@
+document.getElementById('myForm').addEventListener('submit',function(event){
+    event.preventDefault()
+    alert("Form Submitted")
+    const fname = document.getElementById('fname').value;
+    const lname = document.getElementById('lname').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const phoneNumber = document.getElementById('phone-number').value;
+    const password = document.getElementById('password').value;
+    const cpass = document.getElementById('cpass').value;
+    const state = document.getElementById('state').value;
+    const comments = document.getElementById('comments').value;
+
+    if(password != cpass){
+        alert("Password doesn't match.")
+        return
+    }
+
+    if(!fname){
+        alert('You need to enter your name First Name.');
+        return
+    }
+
+    if(!lname){
+        alert("You need to enter your Last Name.")
+        return
+    }
+    
+    if (!age || age<18) {
+        alert('You need to be 18.');
+        return
+    }
+
+
+    const FormData = {
+        fname: fname,
+        lname: lname,
+        age: age,
+        email: email,
+        phoneNumber: phoneNumber,
+        password: password,
+        cpass: cpass,
+        state: state,
+        comments: comments,
+    }
+
+    console.log(FormData);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("get", "submission.json");
+    xhr.setRequestHeader("content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function (){
+        if (xhr.readyState == 4 && xhr.status === 200){
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("myForm").innerHTML = " ";
+        }
+        else if (xhr.readyState === 4){
+            alert ("Error submitting form.");
+        }
+    }
+    xhr.send(JSON.stringify(FormData));
+    alert("Success")
+});
